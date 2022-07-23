be6e728f8249619747d4e2d3a7466fc4d5b8f6a5
walk-ingegg <walkingegg.8814919@gmail.com>
2022-07-23 20:42:15 +0900

    update タスクトレイに常駐

--- open_local_location/main.py
+++ open_local_location/main.py
@@ -1,20 +1,37 @@
+import threading
 from pystray import Icon, Menu, MenuItem
 from PIL import Image
+import keyboard
+import pyperclip
 
 from open_local_location import open_explorer
 
 
+def open_main():
+    file_path = pyperclip.paste()
+    file_path = file_path.strip('"')
+    open_explorer('business', file_path)
+
+
 def quit_app():
+    global app
     app.stop()
 
 
 def run_app():
-    pass
+    global app
+
+    menu = Menu(MenuItem('Quit', quit_app))
+    image = Image.open('open_local_location/icon.png')
+    app = Icon("name", image, "Dropen", menu)
+    app.run()
 
 
-image = Image.open('open_local_location/icon.png')
-menu = Menu(MenuItem('Quit', quit_app), MenuItem('Run', run_app))
-app = Icon(name='test', icon=image, title='pystray App', menu=menu)
-app.run()
+def main():
+    threading.Thread(target=run_app).start()
 
-open_explorer('Syntegrate&vicc Dropbox')
+    keyboard.add_hotkey("ctrl+alt+p", open_main)
+
+
+if __name__ == "__main__":
+    main()
